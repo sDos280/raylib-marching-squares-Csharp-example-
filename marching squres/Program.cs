606e79a63d1cf49641b7507df1bcb80cc57537da
דור שפירא <Dor.Shapira@outlook.com>
2022-08-07 22:44:32 +0300

    adding grid visualization

--- marching squres/Program.cs
+++ marching squres/Program.cs
@@ -1,19 +1,47 @@
 ﻿using Raylib_cs;
+using engine;
 
 namespace marching_squres
 {
     internal class Program
     {
+        const int screenWidth = 800;
+        const int screenHeight = 800;
+
+        const int rows = 20;
+        const int columns = 20;
+
+        const int tile_x = screenWidth / columns;
+        const int tile_y = screenHeight / rows;
+
+        static int[,] grid = new int[columns, rows];
+
         public static void Main()
         {
-            Raylib.InitWindow(800, 480, "Hello World");
+            Raylib.InitWindow(screenWidth, screenHeight, "raylib [other] example - marching squres algorithm");
+
+            Raylib.SetTargetFPS(60);
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    grid[c, r] = Raylib.GetRandomValue(0, 1);
+                }
+            }
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
+                Raylib.ClearBackground(Color.BLACK);
 
-                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < columns; c++)
+                    {
+                        Raylib.DrawCircle(tile_x * c, tile_y * r, 5, grid[c, r] == 1 ? Color.BLACK : Color.WHITE);
+                    }
+                }
 
                 Raylib.EndDrawing();
             }
