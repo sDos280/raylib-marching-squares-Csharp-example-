abbc5f85c319f9181f78013479a0bbaf950a3d38
דור שפירא <Dor.Shapira@outlook.com>
2022-08-07 22:27:55 +0300

    Add project files.

---
+++ marching squres/Program.cs
@@ -0,0 +1,24 @@
+﻿using Raylib_cs;
+
+namespace marching_squres
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
