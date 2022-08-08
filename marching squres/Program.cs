e6ee2d9d0a145800b79ac0b1bbb5a2f743816815
דור שפירא <Dor.Shapira@outlook.com>
2022-08-08 09:35:07 +0300

    adding interpolation logic

--- marching squres/Program.cs
+++ marching squres/Program.cs
@@ -10,19 +10,23 @@ namespace marching_squares
         const int screenWidth = 800;
         const int screenHeight = 800;
 
-        const int rows = 20;
-        const int columns = 20;
+        const int rows = 40;
+        const int columns = 40;
 
-        const int tile_x = screenWidth / columns;
-        const int tile_y = screenHeight / rows;
+        const float tile_x = screenWidth / (columns - 1);
+        const float tile_y = screenHeight / (rows - 1);
 
         static int[,] grid = new int[columns, rows];
+
+        static bool WithInterpolation = true;
 
         public static void Main()
         {
             Raylib.InitWindow(screenWidth, screenHeight, "raylib [other] example - marching squres algorithm");
 
             Raylib.SetTargetFPS(60);
+
+            Raylib.SetRandomSeed(10);
 
             for (int r = 0; r < rows; r++)
             {
@@ -41,7 +45,7 @@ namespace marching_squares
                 {
                     for (int c = 0; c < columns; c++)
                     {
-                        Raylib.DrawCircle(tile_x * c, tile_y * r, 5, grid[c, r] == 1 ? Color.WHITE : Color.BLACK);
+                        Raylib.DrawCircle((int)(c * tile_x), (int)(r * tile_y), 5, grid[c, r] == 1 ? Color.WHITE : Color.BLACK);
                     }
                 }
 
@@ -56,10 +60,33 @@ namespace marching_squares
                         Vector2 buttomleftPoint = new Vector2(c * tile_x, (r + 1) * tile_y);
                         Vector2 buttomrightPoint = new Vector2((c + 1) * tile_x, (r + 1) * tile_y);
 
-                        Vector2 middleUpPoint = (topleftPoint + toprightPoint) / 2;
-                        Vector2 middleDownPoint = (buttomleftPoint + buttomrightPoint) / 2;
-                        Vector2 middleRightPoint = (toprightPoint + buttomrightPoint) / 2;
-                        Vector2 middleLeftPoint = (topleftPoint + buttomleftPoint) / 2;
+                        float topleftPointValue = grid[c, r] + 1;
+                        float toprightPointValue = grid[c + 1, r] + 1;
+                        float buttomleftPointValue = grid[c, r + 1] + 1;
+                        float buttomrightPointValue = grid[c + 1, r + 1] + 1;
+
+                        Vector2 middleUpPoint = new Vector2();
+                        Vector2 middleDownPoint = new Vector2();
+                        Vector2 middleRightPoint = new Vector2();
+                        Vector2 middleLeftPoint = new Vector2();
+
+                        if (WithInterpolation)
+                        {
+                            middleUpPoint = Raymath.Vector2Lerp(topleftPoint, toprightPoint, (1 - topleftPointValue) / (toprightPointValue - topleftPointValue));
+                            middleDownPoint = Raymath.Vector2Lerp(buttomleftPoint, buttomrightPoint, (1 - buttomleftPointValue) / (buttomrightPointValue - buttomleftPointValue));
+                            middleRightPoint = Raymath.Vector2Lerp(toprightPoint, buttomrightPoint, (1 - toprightPointValue) / (buttomrightPointValue - toprightPointValue));
+                            middleLeftPoint = Raymath.Vector2Lerp(topleftPoint, buttomleftPoint, (1 - topleftPointValue) / (buttomleftPointValue - topleftPointValue));
+                        }
+                        else
+                        {
+                            middleUpPoint = (topleftPoint + toprightPoint) / 2;
+                            middleDownPoint = (buttomleftPoint + buttomrightPoint) / 2;
+                            middleRightPoint = (toprightPoint + buttomrightPoint) / 2;
+                            middleLeftPoint = (topleftPoint + buttomleftPoint) / 2;
+                        }
+
+
+                        
 
                         switch (Case)
                         {
